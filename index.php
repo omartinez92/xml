45e82a513f56cebc1625ed371c3ce6b0549c584c
Omar <ericomar81@gmail.com>
2015-03-30 13:59:43 -0600

    Add every array and function for every brand of product

--- index.php
+++ index.php
@@ -8,45 +8,75 @@ require "Classes/PHPExcel.php";
 require 'Classes/PHPExcel/IOFactory.php';
 include 'cartridge.php';
 include 'printer.php';
+include 'monitor.php';
+include 'pc.php';
+include 'notebook.php';
+include 'accesories.php';
+include 'tablet.php';
 
 $files = scandir($path_to_xml);
 $tab=0;
 //print_r($files);
+$file_printer, $file_cartridge, $file_notebook, $file_tablet, $file_monitor, $file_pc, $file_accesories = '';
 foreach ($files as $file) {
+    
 	//unset($objPHPExcel);
     //unset($xml);
 	if (substr($file, strrpos($file, '.')+1) == 'xml') {
         $xml = simplexml_load_file(__DIR__."/XML/$file");
         $product_name 	= strtolower((string)$xml->content->system->product_names->prodname);
         $product_number = (string)$xml->content->system->product_numbers->prodnum;
-
+        
         if(strstr($product_name, 'cartucho') || strstr($product_name, 'tinta') || strstr($product_name, 'cabezal') || strstr($product_name, 'tambor') || strstr($product_name, "tóner") || strstr($product_name, 'cartridge')) :
         	//echo "<div style=\"background-color: red;\">$product_name    con numero:    $product_number</div>";
             $info = cartridge($xml);
+            $info = extract_information($info);
+            $file_cartridge=$info[0].PHP_EOL.$info[1];
+       
         elseif(strstr($product_name, 'impresora')):
             $info = printer($xml);
+            $info = extract_information($info);
+            $file_cartridge=$info[0].PHP_EOL.$info[1];
         elseif(strstr($product_name, 'notebook') || strstr($product_name, 'laptop')):
-        	echo "<div style=\"background-color: green;\">$product_name    con numero:    $product_number</div>";
-
+        	//echo "<div style=\"background-color: green;\">$product_name    con numero:    $product_number</div>";
+            $info = notebook($xml);
+            $info = extract_information($info);
+            $$file_notebook=$info[0].PHP_EOL.$info[1];
         elseif (strstr($product_name, "tablet")):
-        	echo "<div style=\"background-color: blue;\">$product_name    con numero:    $product_number</div>";
-
+        	//echo "<div style=\"background-color: blue;\">$product_name    con numero:    $product_number</div>";
+            $info = tablet($xml);
+            $info = extract_information($info);
+            $$file_tablet=$info[0].PHP_EOL.$info[1];
         elseif (strstr($product_name, 'monitor')):
-        	echo "<div style=\"background-color: pink;\">$product_name    con numero:    $product_number</div>";
-
+        	//echo "<div style=\"background-color: pink;\">$product_name    con numero:    $product_number</div>";
+            $info = monitor($xml);
+            $info = extract_information($info);
+            $$file_monitor=$info[0].PHP_EOL.$info[1];
         elseif (strstr($product_name, 'pc') || strstr($product_name, 'desktop') || strstr($product_name, 'todo-en-uno') || strstr($product_name, 'pavilion') || strstr($product_name, 'stream') || strstr($product_name, 'envy')):
-        	echo "<div style=\"background-color: brown;\">$product_name    con numero:    $product_number</div>";
-
+        	//echo "<div style=\"background-color: brown;\">$product_name    con numero:    $product_number</div>";
+            $info = pc($xml);
+            $info = extract_information($info);
+            $$file_pc=$info[0].PHP_EOL.$info[1];
         else:
-        	echo "<div style=\"background-color: yellow;\">$product_name    con numero:    $product_number</div>";
-
+        	//echo "<div style=\"background-color: yellow;\">$product_name    con numero:    $product_number</div>";
+            $info = accesories($xml);
+            $info = extract_information($info);
+            $$file_accesories=$info[0].PHP_EOL.$info[1];
         endif;
 
 		
     }
 }
 
-
+function extract_information($file){
+    $info_label, $info_value= '';
+    foreach ($info as $label => $value) {
+               $info_label.=$label;
+               $info_value.= $value;
+            }
+    $labels_values=[$info_label, $info_value];
+    return $labels_values
+}
 
 
 ?>
---
+++ monitor.php
@@ -0,0 +1,73 @@
+<?PHP 
+public function monitor($xml){
+
+    $info_printer = array(
+	'prodname' => (string)$xml->content->system->product_names->prodname,
+	'prodbrandname' => prodbrandname,
+	'facet_segment' => facet_segment,
+
+
+	'aiofunctions' => aiofunctions,
+	'laserprnspdbw' => laserprnspdbw,
+	'prntspdftnote' => prntspdftnote,
+	'dutycycle' => dutycycle,
+	'prnttech' => prnttech,
+	'prntcart' => prntcart,
+	'wirelesscap' => wirelesscap,
+	'duplexprnt' => duplexprnt,
+
+
+    'proddes_overview_medium'	=> (string)$xml->content->special_features->product_description->proddes_overview_medium,
+    'ksp_01_headline_short'		=> (string)$xml->content->features->key_selling_points->ksp_01_headline_short,
+    'ksp_01_headline_medium' 	=> (string)$xml->content->features->key_selling_points->ksp_01_headline_medium,
+    'ksp_01_suppt_01_medium'	=> (string)$xml->content->features->key_selling_points->ksp_01_suppt_01_medium,
+    'ksp_01_suppt_02_medium' 	=> (string)$xml->content->features->key_selling_points->ksp_01_suppt_02_medium,
+    'ksp_01_suppt_03_medium' 	=> (string)$xml->content->features->key_selling_points->ksp_01_suppt_03_medium,
+    'ksp_01_suppt_04_medium'	=> (string)$xml->content->features->key_selling_points->ksp_01_suppt_04_medium,
+    'ksp_02_headline_short'		=> (string)$xml->content->features->key_selling_points->ksp_02_headline_short,
+    'ksp_02_headline_medium'	=> (string)$xml->content->features->key_selling_points->ksp_02_headline_medium,
+    'ksp_02_suppt_01_medium' 	=> (string)$xml->content->features->key_selling_points->ksp_02_suppt_01_medium,
+    'ksp_02_suppt_02_medium' 	=> (string)$xml->content->features->key_selling_points->ksp_02_suppt_02_medium,
+    'ksp_02_suppt_03_medium'	=> (string)$xml->content->features->key_selling_points->ksp_02_suppt_03_medium,
+    'ksp_02_suppt_04_medium'	=> (string)$xml->content->features->key_selling_points->ksp_02_suppt_04_medium,
+    'ksp_03_headline_short' 	=> (string)$xml->content->features->key_selling_points->ksp_03_headline_short,
+    'ksp_03_headline_medium' 	=> (string)$xml->content->features->key_selling_points->ksp_03_headline_medium,
+    'ksp_03_suppt_01_medium' 	=> (string)$xml->content->features->key_selling_points->ksp_03_suppt_01_medium,
+    'ksp_03_suppt_02_medium' 	=> (string)$xml->content->features->key_selling_points->ksp_03_suppt_02_medium,
+    'ksp_03_suppt_03_medium'	=> (string)$xml->content->features->key_selling_points->ksp_03_suppt_03_medium,
+    'ksp_03_suppt_04_medium'	=> (string)$xml->content->features->key_selling_points->ksp_03_suppt_04_medium,
+    'ksp_04_headline_short'		=> (string)$xml->content->features->key_selling_points->ksp_04_headline_short,
+    'ksp_04_headline_medium'	=> (string)$xml->content->features->key_selling_points->ksp_04_headline_medium,
+    'ksp_04_suppt_01_medium'	=> (string)$xml->content->features->key_selling_points->ksp_04_suppt_01_medium,
+    'ksp_04_suppt_02_medium' 	=> (string)$xml->content->features->key_selling_points->ksp_04_suppt_02_medium,
+    'ksp_04_suppt_03_medium' 	=> (string)$xml->content->features->key_selling_points->ksp_04_suppt_03_medium,
+    'ksp_04_suppt_04_medium' 	=> (string)$xml->content->features->key_selling_points->ksp_04_suppt_04_medium,
+
+
+
+	'prodname'      => (string)$xml->content->system->product_names->prodname,
+	'prodnum'       => prodnum,
+	'dimenmet'      => dimenmet,
+	'weightmet'     => weightmet,
+	'wrntyfeatures' => wrntyfeatures,
+
+
+	'msgfootnote_01' => (string)$xml->content->features->footnotes->msgfootnote_01,
+	'msgfootnote_02' => (string)$xml->content->features->footnotes->msgfootnote_02,
+	'msgfootnote_03' => (string)$xml->content->features->footnotes->msgfootnote_03,
+	'msgfootnote_04' => (string)$xml->content->features->footnotes->msgfootnote_04,
+	'msgfootnote_05' => (string)$xml->content->features->footnotes->msgfootnote_05,
+	'msgfootnote_06' => (string)$xml->content->features->footnotes->msgfootnote_06,
+	'msgfootnote_07' => (string)$xml->content->features->footnotes->msgfootnote_07,
+	'msgfootnote_08' => (string)$xml->content->features->footnotes->msgfootnote_08,
+	'msgfootnote_09' => (string)$xml->content->features->footnotes->msgfootnote_09,
+	'msgfootnote_10' => (string)$xml->content->features->footnotes->msgfootnote_10,
+	'msgfootnote_11' => (string)$xml->content->features->footnotes->msgfootnote_11,
+	'msgfootnote_12' => (string)$xml->content->features->footnotes->msgfootnote_12,
+	'msgfootnote_13' => (string)$xml->content->features->footnotes->msgfootnote_13,
+	'msgfootnote_14' => (string)$xml->content->features->footnotes->msgfootnote_14,
+	'msgfootnote_15' => (string)$xml->content->features->footnotes->msgfootnote_15,
+	);
+return $info_printer;
+}
+?>
